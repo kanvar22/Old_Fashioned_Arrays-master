f773885be25bdc271d4e8b085b2f7f11f8c971e8
kanvar22 <league@WTS-iMac-11.attlocal.net>
2018-07-12 20:02:52 -0700

    Worked on old arrays

---
+++ src/_01_array_manipulation/Sorting.java
@@ -0,0 +1,20 @@
+package _01_array_manipulation;
+
+public class Sorting {
+
+	public static void sort(String[] testArray) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < testArray.length; i++) {
+		String s = testArray[i];
+		
+		for (int j = 1+i; j < testArray.length; j++) {
+			
+			if (s.compareTo(testArray[j]) > 0) {
+				String swap = testArray[i];
+				test 
+			}
+		}
+		}
+	}
+
+}
